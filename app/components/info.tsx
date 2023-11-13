04299c26e60b100b3f87aedde63ee22167a4921a
JustASunnyDayForU <112472127+JustASunnyDayForU@users.noreply.github.com>
2023-11-13 15:55:31 +0700

    fixed data.name info

--- app/components/info.tsx
+++ app/components/info.tsx
@@ -17,7 +17,7 @@ const Info: React.FC<InfoProps> = (
     const onAddToCart = () => cart.addItem(data);
     return ( 
         <div>
-            <h1 className="text-3xl font-bold text-gray-900">(data.name)
+            <h1 className="text-3xl font-bold text-gray-900">{data.name};
             </h1>
             <div className="mt-3 flex items-end justify-between">
                 <p className="text-2xl text-gray-900">
